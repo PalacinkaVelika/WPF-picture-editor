3705ada786e49ee6833626c7c50ef7fbbb97f3f3
Pepa Bůh <128801641+PalacinkaVelika@users.noreply.github.com>
2024-03-04 14:10:55 +0100

    First commit (empty project)

---
+++ WPF_picture_editor/WPF_picture_editor/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_picture_editor {
+    /// <summary>
+    /// Interakční logika pro App.xaml
+    /// </summary>
+    public partial class App : Application {
+
+    }
+}
