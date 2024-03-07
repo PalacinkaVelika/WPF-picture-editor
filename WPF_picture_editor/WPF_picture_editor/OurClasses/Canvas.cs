83fd364e6f64ddad56f9386542ca7d4e769e2495
Pepa Bůh <128801641+PalacinkaVelika@users.noreply.github.com>
2024-03-07 11:37:33 +0100

    Canvas Class added

---
+++ WPF_picture_editor/WPF_picture_editor/OurClasses/Canvas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WPF_picture_editor.OurClasses {
+    class Canvas {
+        /// <summary>
+        /// Plátno pro kreslení v okně
+        /// </summary>
+
+        // Vlastnosti
+        private WriteableBitmap canvas_image; // Cesta k souboru kam se ukládá
+
+        // Konstruktor
+        public Canvas() {
+            
+        }
+        // Metody
+        public void ChangeCanvasContent() {
+
+        }
+        public void SaveAsPngImage() {
+
+        }
+    }
+}
